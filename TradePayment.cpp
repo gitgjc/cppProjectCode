c6c7b6f0f6fb89110b4b13daf798c47c0f7ab115
Jannielyn <74348655+Jannielyn@users.noreply.github.com>
2020-11-19 21:08:45 +0800

    Update TradePayment.cpp

--- TradePayment.cpp
+++ TradePayment.cpp
@@ -3,8 +3,8 @@
 
 namespace minirisk {
 
-ppricer_t TradePayment::pricer(const std::string& base_ccy) const {
-    return ppricer_t(new PricerPayment(*this, base_ccy));
+ppricer_t TradePayment::pricer(const std::string& base_currency) const {
+    return ppricer_t(new PricerPayment(*this, base_currency));
 }
 
 } // namespace minirisk
