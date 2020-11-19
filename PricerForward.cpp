1f16d64d661c8f4e5b9709c68c898774199dd72a
Jannielyn <74348655+Jannielyn@users.noreply.github.com>
2020-11-19 21:12:47 +0800

    Update PricerForward.cpp

--- PricerForward.cpp
+++ PricerForward.cpp
@@ -8,22 +8,22 @@
 namespace minirisk {
 
 PricerForward::PricerForward(
-    const TradeFXForward& trd, const std::string& base_ccy)
+    const TradeFXForward& trd, const std::string& base_currency)
     : m_amt(trd.quantity()),
       m_strike(trd.strike()),
-      m_ccy1(trd.ccy1()),
-      m_ccy2(trd.ccy2()),
+      m_currency1(trd.currency1()),
+      m_currency2(trd.currency2()),
       m_fixing_date(trd.fixing_date()),
       m_settle_date(trd.settle_date()),
-      m_base_ccy(base_ccy) {}
+      m_base_currency(base_currency) {}
 
 double PricerForward::price(Market& m, const FixingDataServer* fds) const {
-  ptr_disc_curve_t df = m.get_discount_curve(ir_curve_discount_name(m_ccy2));
+  ptr_disc_curve_t df = m.get_discount_curve(ir_curve_discount_name(m_currency2));
   double disc_factor = df->df(m_settle_date);
 
   double fwd_rate = nan<double>();
   if (fds && m.today() >= m_fixing_date) {
-    const auto& fixing_name = fx_spot_name(m_ccy1, m_ccy2);
+    const auto& fixing_name = fx_spot_name(m_currency1, m_currency2);
     if (m.today() > m_fixing_date) {
       // Must contain fixing, otherwise price failure.
       fwd_rate = fds->get(fixing_name, m_fixing_date);
@@ -36,15 +36,15 @@ double PricerForward::price(Market& m, const FixingDataServer* fds) const {
   }
   if (std::isnan(fwd_rate)) {
     // Try to resolve price from forward curve.
-    ptr_fx_fwd_curve_t fwd = m.get_fx_fwd_curve(fx_fwd_name(m_ccy1, m_ccy2));
+    ptr_fx_fwd_curve_t fwd = m.get_fx_fwd_curve(fx_fwd_name(m_currency1, m_currency2));
     fwd_rate = fwd->fwd(m_fixing_date);
   }
   MYASSERT(!std::isnan(fwd_rate), "FX forward or fixing not available " 
       << m_ccy1 << m_ccy2 << " for " << m_fixing_date.to_string());
   MYASSERT(!std::isnan(disc_factor), "Disc factor not available " 
-      << m_ccy1 << m_ccy2 << " for " << m_settle_date.to_string());
+      << m_currency1 << m_currency2 << " for " << m_settle_date.to_string());
   double fx_spot = 
-    m.get_fx_spot(fx_spot_name(m_ccy2, m_base_ccy));
+    m.get_fx_spot(fx_spot_name(m_currency2, m_base_currency));
   return m_amt * disc_factor * (fwd_rate - m_strike) * fx_spot;
 }
 
