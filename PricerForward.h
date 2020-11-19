bc3dd3ac34a7831ef3ed4c18d2167dc6bf5e9cb5
Jannielyn <74348655+Jannielyn@users.noreply.github.com>
2020-11-19 21:12:59 +0800

    Update PricerForward.h

--- PricerForward.h
+++ PricerForward.h
@@ -6,16 +6,16 @@
 namespace minirisk {
 
 struct PricerForward : IPricer {
-  PricerForward(const TradeFXForward& trd, const std::string& base_ccy);
+  PricerForward(const TradeFXForward& trd, const std::string& base_currency);
   virtual double price(Market& m, const FixingDataServer* fds) const;
  private:
   double m_amt;
   double m_strike;
-  std::string m_ccy1;
-  std::string m_ccy2;
+  std::string m_currency1;
+  std::string m_currency2;
   Date m_fixing_date;
   Date m_settle_date;
-  std::string m_base_ccy;
+  std::string m_base_currency;
 };
 
 } // namespace minirisk
