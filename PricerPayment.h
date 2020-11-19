57974e86056414e2f9447757d5c9d4e45671b152
Jannielyn <74348655+Jannielyn@users.noreply.github.com>
2020-11-19 21:10:33 +0800

    Update PricerPayment.h

--- PricerPayment.h
+++ PricerPayment.h
@@ -7,7 +7,7 @@ namespace minirisk {
 
 struct PricerPayment : IPricer
 {
-    PricerPayment(const TradePayment& trd, const std::string& base_ccy);
+    PricerPayment(const TradePayment& trd, const std::string& base_currency);
 
     virtual double price(Market& m, const FixingDataServer* fds) const;
 
@@ -15,7 +15,7 @@ private:
     double m_amt;
     Date   m_dt;
     string m_ir_curve;
-    string m_fx_ccy;
+    string m_fx_currency;
 };
 
 } // namespace minirisk
