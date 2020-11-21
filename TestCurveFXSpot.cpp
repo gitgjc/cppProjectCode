86d70d2adf426f19a826deb42abc8062aab2577c
Jannielyn <74348655+Jannielyn@users.noreply.github.com>
2020-11-21 15:56:22 +0800

    Update LJY

--- TestCurveFXSpot.cpp
+++ TestCurveFXSpot.cpp
@@ -8,21 +8,20 @@
 
 using namespace::minirisk;
 
-void run(const string& risk_factors_file) {
-  std::shared_ptr<const MarketDataServer> mds(
-      new MarketDataServer(risk_factors_file));
+void run(const string& risk_factors_file)
+{
+  std::shared_ptr<const MarketDataServer> mds(new MarketDataServer(risk_factors_file));
   // Init market object
   Date today(2017,8,5);
   Market mkt(mds, today);
 
-  std::vector<std::pair<std::string, std::string>> test_cases = {
-    {"EUR", "USD"}, {"USD", "EUR"}, {"GBP", "USD"}, {"USD", "GBP"}, 
-    {"JPY", "USD"}, {"USD", "JPY"}, {"EUR", "JPY"}, {"GBP", "JPY"}};
-  for (const auto& test_case : test_cases) {
-    CurveFXSpot spot(
-        &mkt, today, fx_spot_name(test_case.first, test_case.second));
-    std::cout << spot.name() << ": " << spot.spot() << std::endl;
-  }
+  std::vector<std::pair<std::string, std::string>> test_cases = 
+  {{"EUR", "USD"}, {"USD", "EUR"}, {"GBP", "USD"}, {"USD", "GBP"}, {"JPY", "USD"}, {"USD", "JPY"}, {"EUR", "JPY"}, {"GBP", "JPY"}};
+  for (const auto& test_case : test_cases)
+    {
+      CurveFXSpot spot(&mkt, today, fx_spot_name(test_case.first, test_case.second));
+      std::cout << spot.name() << ": " << spot.spot() << std::endl;
+    }
 }
 
 int main(int argc, const char **argv)
