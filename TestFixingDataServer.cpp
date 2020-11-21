dbc46b43f2d05023d1bae15190559092cc4d4496
Jannielyn <74348655+Jannielyn@users.noreply.github.com>
2020-11-21 15:53:35 +0800

    Update LJY

--- TestFixingDataServer.cpp
+++ TestFixingDataServer.cpp
@@ -4,16 +4,15 @@
 
 using namespace minirisk;
 
-void run() {
+void run()
+{
   FixingDataServer fds("../data/fixings.txt");
-  MYASSERT(fds.lookup("FX.SPOT.EUR.USD", Date("20170805")).first == 1.1213, 
-      "Wrong value.");
-  MYASSERT(fds.lookup("FX.SPOT.EUR.GBP", Date("20170804")).first == 0.74, 
-      "Wrong value.");
-  MYASSERT(fds.get("FX.SPOT.EUR.USD", Date("20170804")) == 1.1213, 
-      "Wrong value.");
+  MYASSERT(fds.lookup("FX.SPOT.EUR.USD", Date("20170805")).first == 1.1213, "Wrong value.");
+  MYASSERT(fds.lookup("FX.SPOT.EUR.GBP", Date("20170804")).first == 0.74, "Wrong value.");
+  MYASSERT(fds.get("FX.SPOT.EUR.USD", Date("20170804")) == 1.1213, "Wrong value.");
 }
 
-int main() {
+int main()
+{
   run();
 }
