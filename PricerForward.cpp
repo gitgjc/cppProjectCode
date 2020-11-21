cb41a3ab43f0aff7a6d7953e02fb24c05e64009e
lyly0315 <74714249+lyly0315@users.noreply.github.com>
2020-11-21 15:35:35 +0800

    LY

--- PricerForward.cpp
+++ PricerForward.cpp
@@ -8,44 +8,44 @@
 namespace minirisk {
 
 PricerForward::PricerForward(
-    const TradeFXForward& trd, const std::string& base_currency)
+    const TradeFXForward& trd, const std::string& base_ccy)
     : m_amt(trd.quantity()),
       m_strike(trd.strike()),
-      m_currency1(trd.currency1()),
-      m_currency2(trd.currency2()),
+      m_ccy1(trd.ccy1()),
+      m_ccy2(trd.ccy2()),
       m_fixing_date(trd.fixing_date()),
       m_settle_date(trd.settle_date()),
-      m_base_currency(base_currency) {}
+      m_base_ccy(base_ccy) {}
 
 double PricerForward::price(Market& m, const FixingDataServer* fds) const {
-  ptr_disc_curve_t df = m.get_discount_curve(ir_curve_discount_name(m_currency2));
+  ptr_disc_curve_t df = m.get_discount_curve(ir_curve_discount_name(m_ccy2));
   double disc_factor = df->df(m_settle_date);
 
-  double fwd_rate = nan<double>();
+  double forward_rate = nan<double>();
   if (fds && m.today() >= m_fixing_date) {
-    const auto& fixing_name = fx_spot_name(m_currency1, m_currency2);
+    const auto& fixing_name = fx_spot_name(m_ccy1, m_ccy2);
     if (m.today() > m_fixing_date) {
       // Must contain fixing, otherwise price failure.
-      fwd_rate = fds->get(fixing_name, m_fixing_date);
+      forward_rate = fds->get(fixing_name, m_fixing_date);
     } else {
       // Might contain fixing.
       const auto& res = fds->lookup(fixing_name, m_fixing_date);
       if (res.second) 
-        fwd_rate = res.first; 
+        forward_rate = res.first; 
     }
   }
-  if (std::isnan(fwd_rate)) {
+  if (std::isnan(forward_rate)) {
     // Try to resolve price from forward curve.
-    ptr_fx_fwd_curve_t fwd = m.get_fx_fwd_curve(fx_fwd_name(m_currency1, m_currency2));
-    fwd_rate = fwd->fwd(m_fixing_date);
+    ptr_fx_forward_curve_t forward = m.get_fx_forward_curve(fx_forward_name(m_ccy1, m_ccy2));
+    forward_rate = forward->forward(m_fixing_date);
   }
-  MYASSERT(!std::isnan(fwd_rate), "FX forward or fixing not available " 
-      << m_currency1 << m_currency2 << " for " << m_fixing_date.to_string());
+  MYASSERT(!std::isnan(forward_rate), "FX forward or fixing not available " 
+      << m_ccy1 << m_ccy2 << " for " << m_fixing_date.to_string());
   MYASSERT(!std::isnan(disc_factor), "Disc factor not available " 
-      << m_currency1 << m_currency2 << " for " << m_settle_date.to_string());
+      << m_ccy1 << m_ccy2 << " for " << m_settle_date.to_string());
   double fx_spot = 
-    m.get_fx_spot(fx_spot_name(m_currency2, m_base_currency));
-  return m_amt * disc_factor * (fwd_rate - m_strike) * fx_spot;
+    m.get_fx_spot(fx_spot_name(m_ccy2, m_base_ccy));
+  return m_amt * disc_factor * (forward_rate - m_strike) * fx_spot;
 }
 
 } // namespace minirisk
