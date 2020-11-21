da45bb8701a8a47eb00dd3600b73cc63bbfcbc7a
Jannielyn <74348655+Jannielyn@users.noreply.github.com>
2020-11-21 15:51:04 +0800

    Update LJY

--- TradeFXForward.cpp
+++ TradeFXForward.cpp
@@ -1,10 +1,12 @@
 #include "TradeFXForward.h"
 #include "PricerForward.h"
 
-namespace minirisk {
+namespace minirisk
+{
 
-ppricer_t TradeFXForward::pricer(const std::string& base_currency) const {
-  return ppricer_t(new PricerForward(*this, base_currency));
-}
+  ppricer_t TradeFXForward::pricer(const std::string& base_currency) const
+  {
+    return ppricer_t(new PricerForward(*this, base_currency));
+  }
 
 } // namespace minirisk
