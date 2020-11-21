862e45f3861c52fe951577c69782c4f380e4af18
Jannielyn <74348655+Jannielyn@users.noreply.github.com>
2020-11-21 15:23:05 +0800

    Update TradePayment.h

--- TradePayment.h
+++ TradePayment.h
@@ -2,56 +2,57 @@
 
 #include "Trade.h"
 
-namespace minirisk {
-
-struct TradePayment : Trade<TradePayment>
+namespace minirisk
 {
-    friend struct Trade<TradePayment>;
 
-    static const guid_t m_id;
-    static const std::string m_name;
-
-    TradePayment() {}
-
-    void init(const std::string& currency, double quantity, const Date& delivery_date)
+    struct TradePayment : Trade<TradePayment>
     {
-        Trade::init(quantity);
-        m_currency = currency;
-        m_delivery_date = delivery_date;
-    }
+        friend struct Trade<TradePayment>;
 
-    virtual ppricer_t pricer(const std::string& base_currency) const;
+        static const guid_t m_id;
+        static const std::string m_name;
 
-    const string& currency() const
-    {
-        return m_currency;
-    }
+        TradePayment() {}
 
-    const Date& delivery_date() const
-    {
-        return m_delivery_date;
-    }
+        void init(const std::string& currency, double quantity, const Date& delivery_date)
+        {
+            Trade::init(quantity);
+            m_currency = currency;
+            m_delivery_date = delivery_date;
+        }
 
-private:
-    void save_details(my_ofstream& os) const
-    {
-        os << m_currency << m_delivery_date;
-    }
+        virtual ppricer_t pricer(const std::string& base_currency) const;
 
-    void load_details(my_ifstream& is)
-    {
-        is >> m_currency >> m_delivery_date;
-    }
+        const string& currency() const
+        {
+            return m_currency;
+        }
 
-    void print_details(std::ostream& os) const
-    {
-        os << format_label("Currency") << m_currency << std::endl;
-        os << format_label("Delivery Date") << m_delivery_date << std::endl;
-    }
+        const Date& delivery_date() const
+        {
+            return m_delivery_date;
+        }
 
-private:
-    string m_currency;
-    Date m_delivery_date;
-};
+    private:
+        void save_details(my_ofstream& os) const
+        {
+            os << m_currency << m_delivery_date;
+        }
+
+        void load_details(my_ifstream& is)
+        {
+            is >> m_currency >> m_delivery_date;
+        }
+
+        void print_details(std::ostream& os) const
+        {
+            os << format_label("Currency") << m_currency << std::endl;
+            os << format_label("Delivery Date") << m_delivery_date << std::endl;
+        }
+
+    private:
+        string m_currency;
+        Date m_delivery_date;
+    };
 
 } // namespace minirisk
